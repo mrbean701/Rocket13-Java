d4442a1a6c89fc69cd1d8c69465cf0e89aa22ee1
mrbean701 <71635213+mrbean701@users.noreply.github.com>
2021-05-31 19:15:00 +0700

    Add files via upload

---
+++ TestingSystem_Assignment_5/src/com/vti/backend/Exercise1.java
@@ -0,0 +1,24 @@
+package com.vti.backend;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import com.vti.entity.News;
+
+public class Exercise1 {
+	private Scanner sc;
+	private ArrayList<News> listNews;
+	
+	public Exercise1() {
+		sc = new Scanner(System.in);
+		listNews = new ArrayList<News>();
+	}
+	
+	public void question1() {
+		loadMenu();
+	}
+
+	private void loadMenu() {
+		
+	}
+}
---
+++ TestingSystem_Assignment_5/src/com/vti/entity/INews.java
@@ -0,0 +1,9 @@
+package com.vti.entity;
+
+public interface INews {
+	public void display();
+	
+	public float calculate();
+
+
+}
